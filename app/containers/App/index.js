caae1847f1757dac035dfd85862449ca8ed0aefb
MartinGerža <martingerza1@gmail.com>
2018-05-21 13:35:13 +0200

    New page for services

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -16,13 +16,17 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
+import Services from 'containers/Services/Loadable';
 
 export default function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/services" component={Services} />
         <Route component={NotFoundPage} />
+        
+
       </Switch>
     </div>
   );
