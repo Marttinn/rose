98a49d5bd3419f74c6d30cd603ab909df76eac78
MartinGerža <martingerza1@gmail.com>
2018-05-23 17:57:39 +0200

    Service item with delete button

--- app/containers/Services/index.js
+++ app/containers/Services/index.js
@@ -3,12 +3,86 @@ import { graphql, ApolloProvider } from 'react-apollo';
 import gql from 'graphql-tag';
 import { Link } from 'react-router-dom';
 
+
+
+
+export class ServiceItem extends React.Component{
+  constructor(...args){
+    super(...args);
+    this.delete = this.delete.bind(this)
+    console.log('ServiceItem', this.props)
+
+    this.state = {
+      mystyle : {
+        color: 'blue'
+
+      },
+      deleted: false
+    }
+  }
+
+  delete() {
+    const id = this.props.service.id;
+    console.log(id)
+   
+    this.setState({
+      mystyle : {
+        color: 'gray'
+      }
+    })
+
+   this.props.mutate({
+     variables:{
+       identificator:this.props.service.id
+      }
+    }).then((success)=> {
+      this.setState({
+        deleted : true
+      })
+  })
+  
+
+    
+
+    
+    //alert(id);
+  }
+
+  render(){
+    return(
+      this.state.deleted ? null : 
+      <div style={this.state.mystyle}>
+        {this.props.service.id}
+      <span style={{paddingLeft:'10px'}}>{this.props.service.title}</span> 
+      <span style={{paddingLeft:'10px'}}>  {this.props.service.language}</span>  
+        <button onClick={this.delete}>Delete</button>
+      </div>
+  )
+  
+  
+}
+}
+
+
+
+const ServiceItemQL = graphql(gql`
+mutation  deleteService($identificator:ID!){
+  deleteServices(id:$identificator){
+    id
+  }
+}
+`)(ServiceItem)
+
+
+
 export class Services extends React.Component {
   constructor(...args){
     super(...args);
+    
   }
-
+    
   render() {
+    console.log('ahoj kamo', this.props.data.allServiceses)
     return (
     <div>
       This is ServicesPage component!
@@ -16,12 +90,15 @@ export class Services extends React.Component {
       <Link to="/services/new">
         Create new
       </Link>
+      
       </p>
-         
+      <div>   
         {this.props.data.loading ? <span>Loading...</span> : null}
-        {this.props.data.AllServiceses ? this.props.data.AllServiceses.map((s) => <span>{s.text}</span>) : null}
-
-      </div>
+        {this.props.data.allServiceses ? this.props.data.allServiceses.map((s) => <span>
+          <ServiceItemQL service={s} />
+        </span>) : null}
+        </div>
+    </div>
     );
   }
 }
